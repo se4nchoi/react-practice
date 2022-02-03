289e7d1ff722b4ac9372f0a968251b52e7d0f56e
Sean <yhchoi@emgglobal.co.kr>
2022-02-03 15:57:36 +0900

    [Scrimba-React] Add finished React project 1
    - create with CRA
    - try components
    - static css styling

---
+++ scrimba-react/project1-cra/digital-business-card/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../assets/css/Footer.css";
+
+export default function Footer() {
+  return (
+    <section className="footer">
+      <div className="social">
+        <i class="fab fa-github-square"></i>
+        <i class="fab fa-linkedin"></i>
+        <i class="fab fa-facebook-square"></i>
+      </div>
+    </section>
+  );
+}
